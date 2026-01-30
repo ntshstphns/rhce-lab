2bf56bd137db248d42b3c9c887aa428769160ca2
Natasha Stephens <ntshstphns@yahoo.com>
2026-01-29 21:11:38 -0500

    change region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@
 variable "aws_region" {
   description = "AWS Region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "project_name" {
